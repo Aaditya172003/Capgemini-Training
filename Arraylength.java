79a49c2a7fe937510cbe3f33e50676642c173bf6
Aaditya172003 <165695847+Aaditya172003@users.noreply.github.com>
2024-09-03 09:18:18 +0530

    Array
    To find the length of an Array

---
+++ Arraylength.java
@@ -0,0 +1,6 @@
+public class Arraylength {
+    public static void main(String args[]){
+        int rollno[] = new int[50];
+        System.out.println(rollno.length);                  // we get the length in integer value
+    }
+}
